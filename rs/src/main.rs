205844d8c218c29b078d56821b55f0e8afc5dbb9
Kyosuke Fujimoto <kyoro.f@gmail.com>
2024-03-01 08:07:30 +0900

    Add Rust sample project

---
+++ rs/src/main.rs
@@ -0,0 +1,27 @@
+const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+fn main() {
+    println!("Hello, rs! [Version: {}]", APP_VERSION);
+}
+
+pub struct User {
+    id: usize
+}
+
+impl User {
+    pub fn new() -> User {
+        User { id: 0 }
+    }
+
+    pub fn print(&self) {
+        println!("User: id = {}", self.id)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn foo() {
+        assert_eq!(2 * 3, 5)
+    }
+}
